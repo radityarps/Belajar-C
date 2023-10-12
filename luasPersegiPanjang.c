95fdab2d20244c5d2b0742cbe98b9204678ffeec
radityarps <radityarafifps@gmail.com>
2023-10-12 21:11:10 +0700

    first commit

---
+++ luasPersegiPanjang.c
@@ -0,0 +1,21 @@
+/*
+Identitas Program : Luas Persegi Panjang
+Nama : Raditya Rafif Pratama Sasmita
+NIM : 3.34.23.2.18
+Latihan Implementasi Flowchart Luas Persegi Panjang
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    /* input */
+    int panjang = 31;
+    int lebar = 27;
+
+    /* proses */
+    int luas = panjang * lebar;
+
+    /* output */
+    printf("Luas Persegi Panjang = %d\n", luas);
+}
