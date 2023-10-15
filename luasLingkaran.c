aaf616c65b0284f29b9e27408e0d09ab8991a982
radityarps <radityarafifps@gmail.com>
2023-10-15 20:34:12 +0700

    readeble code

--- luasLingkaran.c
+++ luasLingkaran.c
@@ -11,9 +11,11 @@ int main()
     /* input */
     float pi = 3.14;
     int r = 15;
+
     /* proses */
     float luasLingkaran = pi * r * r;
     float kelilingLingkaran = 2 * pi * r;
+
     /* output */
     printf("Luas Lingkaran : %f\n", luasLingkaran);
     printf("Keliling Lingkaran : %f", kelilingLingkaran);
